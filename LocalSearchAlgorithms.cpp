1b4786173786453d33aeb2d3cfa62a5412f3ec8c
Jan Wawruszczak <janwawruszczak1998@gmail.com>
2019-11-28 00:29:09 +0100

    improved SA

--- LocalSearchAlgorithms.cpp
+++ LocalSearchAlgorithms.cpp
@@ -12,44 +12,89 @@ void SA(Graph *g){
     int result = random_path(solution, g);   //pierwsze losowe rozwiazanie
     auto permutation(solution);
     auto next_step(permutation);
-    int iteration = 0;
-    double temperature = 1e9;
+    int step_val = calculate_objective(next_step, g);
+
+    int stagnation = 0; //wskaznik stagnacji
+
+    int iteration = 0;  //liczba iteracji w ramach jednego chlodzenia i wyzazania
+    double temperature = 1e9;   //temperatura poczatkowa
 
     for(int num_of_try = 0; num_of_try < 100; ++num_of_try) {
         cout << num_of_try << "%\n";
         while(temperature > 0.1){
 
             iteration++;
-            int rep = g->get_rank()*g->get_rank();
+            int rep = g->get_rank()*g->get_rank();  //liczba obrotow = n^2
             next_step = permutation;
+            step_val = calculate_objective(next_step, g);   //wartosc kroku
 
             while(rep-->0){
+                int f_pos = 0;  //wyznaczenie pierwszego miejsca swapa
+                int s_pos = 0;  //wyznaczenie drugiego miejsca swapa
+                do{
+                    f_pos= rand() % g->get_rank();
+                    s_pos = rand() % g->get_rank();
+                }while(f_pos == s_pos);
+                if(f_pos > s_pos) std::swap(f_pos, s_pos);
 
-                int pos1 = rand() % g->get_rank();
-                int pos2 = rand() % g->get_rank();
+                //wyznaczenie nastepnikow i poprzednikow miejsc swapa
+                int pre_f_pos = f_pos - 1;
+                int next_f_pos = f_pos + 1;
+                if(f_pos == 0) pre_f_pos = g->get_rank() - 1;
 
-                std::swap(next_step[pos1], next_step[pos2]);
-                int step_val = calculate_objective(next_step, g);
-                int difference = step_val - result;
+                int pre_s_pos = s_pos - 1;
+                int next_s_pos = s_pos + 1;
+                if(s_pos == g->get_rank() - 1) next_s_pos = 0;
 
 
-                if(step_val < result){
+                //sprawdzenie wartosci kroku
+                if(s_pos - f_pos == 1 || s_pos == g->get_rank() - 1 || f_pos == 0){ //krancowe przypadki
+                    std::swap(next_step[f_pos], next_step[s_pos]);
+                    step_val = calculate_objective(next_step, g);
+                }
+                else{   //ogolny przypadek
+                    step_val = step_val
+                               - g->get_matrix()[next_step[pre_f_pos]][next_step[f_pos]]
+                               - g->get_matrix()[next_step[f_pos]][next_step[next_f_pos]]
+                               - g->get_matrix()[next_step[pre_s_pos]][next_step[s_pos]]
+                               - g->get_matrix()[next_step[s_pos]][next_step[next_s_pos]]
+                               + g->get_matrix()[next_step[pre_f_pos]][next_step[s_pos]]
+                               + g->get_matrix()[next_step[s_pos]][next_step[next_f_pos]]
+                               + g->get_matrix()[next_step[pre_s_pos]][next_step[f_pos]]
+                               + g->get_matrix()[next_step[f_pos]][next_step[next_s_pos]];
+                    std::swap(next_step[f_pos], next_step[s_pos]);
+                }
+
+                int difference = step_val - result; //roznica rozwiazan najlepszego i aktualnego
+
+
+                if(step_val < result){  //jesli poprawilismy, wyzeruj wskaznik stagnacji i aktualizuj rezultat
+                    stagnation = 0;
                     result = step_val;
                     solution = next_step;
                 }
+                else{   //jesli nie poprawilismy, to zwieksz wskaznik stagnacji
+                    stagnation++;
+                    if(stagnation < g->get_rank()) temperature = (1e9)/2;   //jesli stagnacja jest duza, to zwieksz temperature
+                }
+
+                //decyzja o przyjeciu/odrzuceniu rozwiazania
                 if(difference < 0 || (difference >0 &&  get_probability(difference, temperature) > ( (double) rand() / (RAND_MAX)) + 1) ){
                     permutation = next_step;
                 }
 
 
             }
-            temperature = cooling(temperature, iteration);
+            temperature = cooling(temperature, iteration);  //akutalizacja temperatury
+            if(iteration > g->get_rank()*g->get_rank()) break; //jesli zbyt dlugo siedzimy w jednym miejscu
         }
 
-
+        //losowanie nowego punktu startu i reset zmiennych
         random_path(permutation, g);
         next_step = permutation;
+        step_val = calculate_objective(permutation, g);
         iteration = 0;
+        stagnation = 0;
         temperature = 1e9;
     }
 
@@ -71,7 +116,7 @@ void TS(Graph *g){
 
     //algorytm przeszukiwania z zakazami
     for(int num_of_try = 0; num_of_try < 200; ++num_of_try) {
-        cout << num_of_try/200 << "%\n";
+        std::cout << num_of_try/2 << "%\n";
         for (int step = 0; step < 15*g->get_rank(); ++step) {
             int next_step_val = std::numeric_limits<int>::max();
 
